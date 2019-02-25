58c2c3e46eaac60897a7a5187664320e186aecdd
Danas <danas.mikelionis@gmail.com>
2019-02-25 10:26:17 +0200

    Initial commit

---
+++ src/main/java/com/vtvpmc/DanasMikelionis/ReactApp/ReactAppApplication.java
@@ -0,0 +1,13 @@
+package com.vtvpmc.DanasMikelionis.ReactApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vtvpmc/DanasMikelionis/ReactApp/ReactAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vtvpmc.DanasMikelionis.ReactApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
